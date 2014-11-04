850d9d17fc9da69cb8641c6b7ab8a2dce21be78f
miracode <rascatim@gmail.com>
2014-11-04 12:24:48 -0800

    startings of app without full user constraints

--- imagr_site/imagr/models.py
+++ imagr_site/imagr/models.py
@@ -2,12 +2,17 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser
 
 PUBLISHED_CHOICES = (
     ('private', 'This is private'),
     ('shared', 'This is shared'),
     ('public', 'This is public'),
 )
+
+
+class ImagrUser(AbstractBaseUser):
+    pass
 
 
 class Photo(models.Model):
@@ -17,6 +22,7 @@ class Photo(models.Model):
     date_modified = models.DateTimeField('date modified')
     date_published = models.DateTimeField('date published')
     published = models.CharField(max_length=8, choices=PUBLISHED_CHOICES)
+    owner = models.ForeignKey(ImagrUser, verbose_name="Owner of photo")
 
 
 class Album(models.Model):
@@ -26,5 +32,6 @@ class Album(models.Model):
     date_modified = models.DateTimeField('date modified')
     date_published = models.DateTimeField('date published')
     published = models.CharField(max_length=8, choices=PUBLISHED_CHOICES)
-    cover_photo = models.ForeignKey(Photo)
+    owner = models.ForeignKey(ImagrUser, verbose_name="Owner of album")
+    cover_photo = models.ForeignKey(Photo, related_name="cover_photo")
     photos = models.ManyToManyField(Photo, verbose_name="photos in album")
